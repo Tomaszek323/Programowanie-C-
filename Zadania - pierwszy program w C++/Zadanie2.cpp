cde584d7db8866cb04ce41b4e7e145881b6a8f8a
Aleks Gajewski <ag31127@zpsb.pl>
2026-03-08 10:02:49 +0100

    Dodanie zadania drugiego

---
+++ Zadania - pierwszy program w C++/Zadanie2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a = 10;
+	int b = 6;
+	
+
+cout << "Suma: " << a + b << endl;
+cout << "roznica: " << a - b << endl;
+cout << "iloczyn: " << a * b << endl;
+
+return 0;
+}
